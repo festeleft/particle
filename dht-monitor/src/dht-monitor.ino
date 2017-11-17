34d93e3ffcabfe6b9036d8cd5bfad82e6af4baee
psahota <paul.sahota@software.dell.com>
2017-11-17 10:24:18 -0500

    refactors commands and adds particle variables

--- dht-monitor/src/dht-monitor.ino
+++ dht-monitor/src/dht-monitor.ino
@@ -16,16 +16,17 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-int g_observing = 1; // CHANGEME: set to 0 to have the unit not log after a restart/power failure
-int g_sample_frequency = 60000; // CHANGEME: sample frequency set to 1 minute
+int g_publishing = 0; // CHANGEME: set to 0 to have the unit not log after a restart/power failure
+int g_sample_frequency = 5000; // sample once every 5 minutes
   // TODO mechanism to dynamically update the sample frequency
   // TODO Idea of temporarily ramping up the frequency while someone is
   //      actively inspecting the data. Could be a simple poll instruction.
+double g_humidity = NAN; // Particle variable
+double g_temperature = NAN; // Particle variable
 
-
-int pollControl(String command) {
-   if (command == "start-observations") {
-        g_observing = 1;
+int publishCommand(String command) {
+   if (command == "start") {
+        g_publishing = 1;
         bool success;
         success = Particle.publish("info", "starting observations");
         if (!success) {
@@ -33,20 +34,20 @@ int pollControl(String command) {
         }
         return 1;
    }
-   else if (command == "stop-observations") {
+   else if (command == "stop") {
         bool success;
         success = Particle.publish("info", "stopping observations");
         if (!success) {
           Serial.println("publish stop info failed!!!!");
         }
         Serial.println("stopping...");
-        g_observing = 0; // set the flag to not collect observations
+        g_publishing = 0; // set the flag to not collect observations
         return 1;
    }
 
     Serial.println("Error (illegal CMD).");
     bool success;
-    success = Particle.publish("error", "illegal poll command");
+    success = Particle.publish("error", "illegal command");
     if (!success) {
       Serial.println("publish illegal poll command failed!!!!");
     }
@@ -54,10 +55,11 @@ int pollControl(String command) {
     return 0;
 }
 
-void publishObservations(float humidity, float temperature) {
+void publish(float humidity, float temperature) {
     char working_buffer[64];
     String data;
     data = "[";
+    // TODO code simple behaviour
     sprintf(working_buffer,
         "{metric: \"temperature\", value: \"%.01f\", unit: \"Celcius\"}",
         temperature);
@@ -79,8 +81,10 @@ void setup() {
     Serial.begin(9600);      // open the serial port at 9600 bps:
     Serial.println("\nSetup...");      // prints another carriage return
 
-    // Register our Spark function here
-    Spark.function("poll", pollControl);
+    Particle.variable("temperature", &g_temperature, DOUBLE);
+    Particle.variable("humidity", &g_humidity, DOUBLE);
+
+    Particle.function("publish", publishCommand);
 
     dht.begin();
 }
@@ -96,41 +100,41 @@ void loop() {
     String serial_data;
     char working_buffer[64];
 
-    if (g_observing) {
-        if (millis() <= g_next_observation) {
-          return;
-        }
-
-        g_next_observation = millis() + g_sample_frequency;
-        failure = 0; // clear failure flag before attempt to read the sensors
-        humidity = dht.getHumidity();
-        delay(50);
-        temperature = dht.getTempCelcius();
-
-        if (temperature == NAN) {
-            Serial.println("FAILED to read Temperature");
-            Particle.publish("error", "FAILED to read Temperature");
-            failure |= ERROR_READING_TEMPERATURE;
-        }
-        if (humidity == NAN) {
-            Serial.println("FAILED to read Humidity");
-            Particle.publish("error", "FAILED to read Humidity");
-            failure |= ERROR_READING_HUMIDITY;
-        }
-
-        // optional serial dump of the information
-        serial_data = "";
-        sprintf(working_buffer, "temperature=%2f, humidity=%2f\n",
-                temperature, humidity);
-        serial_data += working_buffer;
-        Serial.println(serial_data);
-
-        if (!failure) {
-            // Success
-            publishObservations(humidity, temperature);
-        }
+    if (millis() <= g_next_observation) {
+      delay(10); // wait a few milliseconds before looping around again
+      return;
     }
-    else {
-        delay(10); // wait a few milliseconds before looping around again
+
+    g_next_observation = millis() + g_sample_frequency;
+    failure = 0; // clear failure flag before attempt to read the sensors
+    humidity = dht.getHumidity();
+    delay(50);
+    temperature = dht.getTempCelcius();
+
+    if (temperature == NAN) {
+        Serial.println("FAILED to read Temperature");
+        Particle.publish("error", "FAILED to read Temperature");
+        failure |= ERROR_READING_TEMPERATURE;
+    }
+    if (humidity == NAN) {
+        Serial.println("FAILED to read Humidity");
+        Particle.publish("error", "FAILED to read Humidity");
+        failure |= ERROR_READING_HUMIDITY;
+    }
+
+    // optional serial dump of the information
+    serial_data = "";
+    sprintf(working_buffer, "temperature=%2f, humidity=%2f\n",
+                temperature, humidity);
+    serial_data += working_buffer;
+    Serial.println(serial_data);
+
+    // update global (particle) variables
+    g_temperature = (double) temperature;
+    g_humidity = (double) humidity;
+
+    if (g_publishing) {
+        // Success
+        publish(humidity, temperature);
     }
 }
